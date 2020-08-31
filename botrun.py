011489235dcd55cc77c6e18b58e2fa6380410c1d
tigersharkpr13 <pater4skyrim.dc@gmail.com>
2020-08-31 15:20:38 -0400

    bot events and status changes

--- botrun.py
+++ botrun.py
@@ -1,10 +1,36 @@
 import os
-
+import discord
+import random
 from discord.ext import commands
 
 token = open('token', 'r').read()
 bot = commands.Bot(command_prefix='$')
 bot.remove_command('help')
+
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name='$help'))
+    print('Inari is ready B)')
+
+
+@bot.event
+async def on_message(message: discord.Message):
+    if message.author.bot:
+        return
+    if message.content.startswith('$'):
+        rand = random.randint(0, 20)
+        GoT = [13, 14, 15, 16]
+        Minecraft = [3, 4, 5, 6]
+        if rand in GoT:
+            await bot.change_presence(
+                status=discord.Status.online, activity=discord.Game(name='Ghost Of Tsushima'))
+        if rand in Minecraft:
+            await bot.change_presence(
+                status=discord.Status.online, activity=discord.Game(name='Minecraft'))
+        if rand not in GoT and Minecraft:
+            await bot.change_presence(
+                status=discord.Status.online, activity=discord.Game(name='$help'))
 
 
 @bot.command()
